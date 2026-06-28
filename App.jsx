113cb089434b3021eb5dd4aa792bc51a1bf8d467
Vedika Bhatt <bhattvedika74@gmail.com>
2026-06-28 15:34:28 +0530

    Add files via upload
    Code for the React project.

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import Stopwatch from './Stopwatch.jsx';
+function App() {
+  return (
+    <div className="App">
+      <h1>This is a Stopwatch</h1>
+      <Stopwatch/>
+    </div>
+  );
+}
+export default App;
+// self explanatory page. 
